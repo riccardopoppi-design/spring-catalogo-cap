2197160a745a7ad88a1f4b7ef084e02835de0fe5
Riccardo Poppi <u23795@gruppogpi.net>
2026-06-09 09:34:50 +0200

    Task 1: Configurato il Bean ModelMapper in MvcConfig

---
+++ src/main/java/com/riccardopoppi/catalogo_cap/config/MvcConfig.java
@@ -0,0 +1,35 @@
+package com.riccardopoppi.catalogo_cap.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    // Questo Bean dice a Spring di rendere disponibile il ModelMapper in tutto il progetto
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        exposeDirectory("uploads", registry);
+    }
+
+    private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
+        Path uploadDir = Paths.get(dirName);
+        String uploadPath = uploadDir.toFile().getAbsolutePath();
+        
+        if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
+        
+        registry.addResourceHandler("/" + dirName + "/**")
+                .addResourceLocations("file:/" + uploadPath + "/");
+    }
+}
